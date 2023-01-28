ecaffe0afe42207d4a3cb34cf7b3516a59cec1c3
Kirill <kerya021003@gmail.com>
2023-01-28 23:46:37 +0300

    Second push

---
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.scheduleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//efwgwefwауац1е3
+//tewgewt
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
