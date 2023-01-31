c32abdd7589b162c51f26e89c57fa7111c81dba0
Kirill <kerya021003@gmail.com>
2023-01-31 20:32:06 +0300

    кнопки с расписанием

--- app/src/main/java/com/example/scheduleapp/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.scheduleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void GoToSchedule(View V){
+        Intent intent = new Intent(this,ScheduleCheckandedit.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/scheduleapp/activity_schedule_check.java
@@ -0,0 +1,14 @@
+package com.example.scheduleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_schedule_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_check);
+    }
+}
