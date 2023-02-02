1b779dd0ad4c70105c44625ad5796af03301b59a
Alex42 <alexcrazy42@mail.ru>
2023-02-02 22:35:46 +0300

    Last commit

--- app/src/main/java/com/example/scheduleapp/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -5,16 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void GoToSchedule(View V){
-        Intent intent = new Intent(this,activity_schedule.class);
+        Intent intent = new Intent(this, activity_schedule.class);
         startActivity(intent);
     }
+
+    public void GoToSettings(View V)
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
 }
