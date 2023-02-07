99697f1699c9cdfd2d7de889d7faf41766a715d2
Alex42 <alexcrazy42@mail.ru>
2023-02-08 00:45:29 +0300

    navigation bar

--- app/src/main/java/com/example/scheduleapp/MainActivity.java
+++ app/src/main/java/com/example/scheduleapp/MainActivity.java
@@ -1,34 +1,61 @@
 package com.example.scheduleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.example.scheduleapp.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void GoToSchedule(View V){
-        Intent intent = new Intent(this, ScheduleActivity.class);
-        startActivity(intent);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MainFragment());
+
+        binding.bottomNavigationView2.setOnItemSelectedListener(item ->
+        {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new MainFragment());
+                    break;
+                case R.id.schedule:
+                    replaceFragment(new ScheduleFragment());
+                    break;
+                case R.id.teachers:
+                    replaceFragment(new TeacherFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+            return true;
+        });
     }
 
-    public void GoToSettings(View V)
+    private void replaceFragment(Fragment fragment)
     {
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 
-    public void GoToTeachers(View V){
-        Intent intent = new Intent(this, TeacherActivity.class);
-        startActivity(intent);
-    }
+
+
+
+
 
 }
